fe6c6a2372541a4256c41f862118d377cfbea172
Simon Kluchko <simon@Users-MacBook-Pro-2.local>
2021-11-23 12:16:17 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [calc, setCalc] = useState(''),
+    ops = ['/', '*', '+', '-', '.'];
+
+  const checkingOps = () => {
+    // if the value we've typed is an operator and the last value we've typed was an operator 
+    if (ops.includes(calc) && ops.includes(calc.slice(-1))) return;
+  }
+
+  const updateCalc = value => {
+    checkingOps();
+    setCalc(calc + value);
+  }
+
+  // calculating the values by clicking the equal sign
+  const calculate = () => {
+    checkingOps();
+    if (calc) setCalc(eval(calc).toString());
+  }
+
+  const clearCalc = () => {
+    if (calc) {
+      setCalc('');
+    } else {
+      return;
+    }
+  }
+
+  /* here I've created function that create buttons from 1 to 9,
+   instead of typing them manually */
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button
+          onClick={() => updateCalc(i.toString())}
+          key={i}>
+          {i}
+        </button>
+      )
+    }
+
+    return digits;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          {calc || '0'}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+
+          <button onClick={clearCalc}>C</button>
+        </div>
+
+
+        <div className="digits">
+          {createDigits()}
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+
+
     </div>
   );
 }
